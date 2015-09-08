f031742e3b31d9eadaac44f68e4a3267b350d1af
Ahsan Ellahi <ahsan.ellahi@square63.com>
2015-09-08 13:10:07 +0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blackvault::Application.config.secret_token = '3a528ebe3e21f8304f25b4c9972e37b6b1e47f8aa5cf02ae847cdec4aed3c5f612b4df952b4d5bb52a44b6b905cc361ba60235248d1dda7023084ead47f44bba'
