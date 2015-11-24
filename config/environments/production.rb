166d45cd282386ee134f1d4ab47190333a490fec
Ahsan Ellahi <ahsan.ellahi896@gmail.com>
2015-11-24 18:20:01 +0500

    Production.rb: Changed host of sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Blackvault::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { :host => 'shoppy-s63.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'www.blackvaultinvestments.com' }
   ActionMailer::Base.smtp_settings = {
     :address        => "smtp.sendgrid.net",
     :port           => "25",
