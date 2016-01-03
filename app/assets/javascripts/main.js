1631e3756ee3190fbe224550af6cf63cbae86a85
Ahsan Ellahi <ahsan.ellahi896@gmail.com>
2016-01-03 19:54:29 +0500

    Home: Edit some styling and removed jquery errors

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -39,7 +39,8 @@ dp(document).ready(function() {
     //QUOTE SLIDE
     dp("#quote-slider").sudoSlider({
         customLink: 'a.quoteLink',
-        speed: 425,
+        speed: 3000,
+        auto: true,
         prevNext: true,
         responsive: true,
         prevHtml: '<a href="#" class="quote-left-indicator"><i class="fa fa-angle-left"></i></a>',
@@ -50,7 +51,7 @@ dp(document).ready(function() {
         updateBefore: true
     });
     // TOOTLTIP
-    dp("[data-toggle='tooltip']").tooltip();
+    // dp("[data-toggle='tooltip']").tooltip();
     //  Responsive layout, resizing the items
     if (dp('.client-slider').length > 0) {
         dp('.client-slider').carouFredSel({
@@ -84,147 +85,146 @@ dp(document).ready(function() {
     var data_address = dp("#map").attr('data-address');
     var color = dp("#map").attr('data-color');
     var saturation = 100;
-    alert(1);
-    $("#map").gmap3({
-        map: {
-            options: {
-                center: [lats, lngs],
-                zoom: 15,
-                mapTypeId: "style1",
-                navigationControl: 0,
-                scrollwheel: false,
-                zoomControl: 0,
-                disableDefaultUI: true,
-                streetViewControl: 0,
-                draggable: 0,
-            }
-        },
-        styledmaptype: {
-            id: "style1",
-            options: {
-                name: "Style 1"
-            },
-            styles: [{
-                featureType: "landscape",
-                stylers: [{
-                    hue: "#000"
-                }, {
-                    saturation: -100
-                }, {
-                    lightness: 40
-                }, {
-                    gamma: 1
-                }]
-            }, {
-                featureType: "road.highway",
-                stylers: [{
-                    hue: color
-                }, {
-                    saturation: saturation
-                }, {
-                    lightness: 20
-                }, {
-                    gamma: 1
-                }, {
-                    visibility: "simplified"
-                }]
-            }, {
-                featureType: "road.arterial",
-                stylers: [{
-                    hue: color
-                }, {
-                    saturation: saturation
-                }, {
-                    lightness: 20
-                }, {
-                    gamma: 1
-                }, {
-                    visibility: "simplified"
-                }]
-            }, {
-                featureType: "road.local",
-                stylers: [{
-                    hue: color
-                }, {
-                    saturation: saturation
-                }, {
-                    lightness: 50
-                }, {
-                    gamma: 1
-                }, {
-                    visibility: "simplified"
-                }]
-            }, {
-                featureType: "water",
-                stylers: [{
-                    hue: "#000"
-                }, {
-                    saturation: -100
-                }, {
-                    lightness: 15
-                }, {
-                    gamma: 1
-                }]
-            }, {
-                featureType: "poi",
-                stylers: [{
-                    hue: "#000"
-                }, {
-                    saturation: -100
-                }, {
-                    lightness: 25
-                }, {
-                    gamma: 1
-                }, {
-                    visibility: "simplified"
-                }]
-            }, {
-                featureType: "road",
-                elementType: "labels",
-                stylers: [{
-                    visibility: "off"
-                }]
-            }]
-        },
-        marker: {
-            values: [{
-                latLng: [lats, lngs],
-                data: data_address,
-                options: {
-                    icon: "images/map-marker.png"
-                }
-            }],
-            options: {
-                draggable: false
-            },
-            events: {
-                click: function(marker, event, context) {
-                    var map = dp(this).gmap3("get"),
-                        infowindow = dp(this).gmap3({
-                            get: {
-                                name: "infowindow"
-                            }
-                        });
-                    if (infowindow) {
-                        infowindow.open(map, marker);
-                        infowindow.setContent(context.data);
-                    } else {
-                        dp(this).gmap3({
-                            infowindow: {
-                                anchor: marker,
-                                options: {
-                                    content: context.data
-                                }
-                            }
-                        });
-                    }
-                }
+    // $("#map").gmap3({
+    //     map: {
+    //         options: {
+    //             center: [lats, lngs],
+    //             zoom: 15,
+    //             mapTypeId: "style1",
+    //             navigationControl: 0,
+    //             scrollwheel: false,
+    //             zoomControl: 0,
+    //             disableDefaultUI: true,
+    //             streetViewControl: 0,
+    //             draggable: 0,
+    //         }
+    //     },
+    //     styledmaptype: {
+    //         id: "style1",
+    //         options: {
+    //             name: "Style 1"
+    //         },
+    //         styles: [{
+    //             featureType: "landscape",
+    //             stylers: [{
+    //                 hue: "#000"
+    //             }, {
+    //                 saturation: -100
+    //             }, {
+    //                 lightness: 40
+    //             }, {
+    //                 gamma: 1
+    //             }]
+    //         }, {
+    //             featureType: "road.highway",
+    //             stylers: [{
+    //                 hue: color
+    //             }, {
+    //                 saturation: saturation
+    //             }, {
+    //                 lightness: 20
+    //             }, {
+    //                 gamma: 1
+    //             }, {
+    //                 visibility: "simplified"
+    //             }]
+    //         }, {
+    //             featureType: "road.arterial",
+    //             stylers: [{
+    //                 hue: color
+    //             }, {
+    //                 saturation: saturation
+    //             }, {
+    //                 lightness: 20
+    //             }, {
+    //                 gamma: 1
+    //             }, {
+    //                 visibility: "simplified"
+    //             }]
+    //         }, {
+    //             featureType: "road.local",
+    //             stylers: [{
+    //                 hue: color
+    //             }, {
+    //                 saturation: saturation
+    //             }, {
+    //                 lightness: 50
+    //             }, {
+    //                 gamma: 1
+    //             }, {
+    //                 visibility: "simplified"
+    //             }]
+    //         }, {
+    //             featureType: "water",
+    //             stylers: [{
+    //                 hue: "#000"
+    //             }, {
+    //                 saturation: -100
+    //             }, {
+    //                 lightness: 15
+    //             }, {
+    //                 gamma: 1
+    //             }]
+    //         }, {
+    //             featureType: "poi",
+    //             stylers: [{
+    //                 hue: "#000"
+    //             }, {
+    //                 saturation: -100
+    //             }, {
+    //                 lightness: 25
+    //             }, {
+    //                 gamma: 1
+    //             }, {
+    //                 visibility: "simplified"
+    //             }]
+    //         }, {
+    //             featureType: "road",
+    //             elementType: "labels",
+    //             stylers: [{
+    //                 visibility: "off"
+    //             }]
+    //         }]
+    //     },
+    //     marker: {
+    //         values: [{
+    //             latLng: [lats, lngs],
+    //             data: data_address,
+    //             options: {
+    //                 icon: "images/map-marker.png"
+    //             }
+    //         }],
+    //         options: {
+    //             draggable: false
+    //         },
+    //         events: {
+    //             click: function(marker, event, context) {
+    //                 var map = dp(this).gmap3("get"),
+    //                     infowindow = dp(this).gmap3({
+    //                         get: {
+    //                             name: "infowindow"
+    //                         }
+    //                     });
+    //                 if (infowindow) {
+    //                     infowindow.open(map, marker);
+    //                     infowindow.setContent(context.data);
+    //                 } else {
+    //                     dp(this).gmap3({
+    //                         infowindow: {
+    //                             anchor: marker,
+    //                             options: {
+    //                                 content: context.data
+    //                             }
+    //                         }
+    //                     });
+    //                 }
+    //             }
 
-            }
-        }
-    });
+    //         }
+    //     }
+    // });
     //Bootstrap Tooltip
-    dp('a[data-toggle="tooltip"]').tooltip();
+    // dp('a[data-toggle="tooltip"]').tooltip();
     //NIVOLightbox
     if (dp.fn.nivoLightbox) {
         dp('.popup').nivoLightbox({
